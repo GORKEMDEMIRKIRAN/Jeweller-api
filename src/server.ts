6a2094b292dc431adf5dae75654f4c1a8d5bd42d
Görkem DEMİRKIRAN <121108425+GORKEMDEMIRKIRAN@users.noreply.github.com>
2025-10-08 22:53:56 +0300

    vercel.json created.

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@
 import app from "./app.js";
 import { config } from './config/config.js';
 
-
 if (process.env.NODE_ENV !== 'production') {
     app.listen(config.port, () => {
         console.log(`GOLD BORSE http://localhost:${config.port} address working....`);
     });
 }
 
-
 export default app;
+
+
+
